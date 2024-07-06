9248484946e3362766e6eddd82a0615bd5aa3fa3
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-07-06 08:48:02 +0530

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -3,6 +3,6 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 @Client.on_message(filters.private & filters.command(["refer"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
-       		[ [ InlineKeyboardButton(" 📲   ᴄʟɪᴄᴋ  ᴍᴇ  ᴛᴏ  ʀᴇꜰᴇʀ   📲 " ,url=f"https://telegram.me/share/url?url=https://telegram.me/Shizuka_Filter_Bot?start={message.from_user.id}") ]   ])
+       		[ [ InlineKeyboardButton(" 📲   ᴄʟɪᴄᴋ  ᴍᴇ  ᴛᴏ  ʀᴇꜰᴇʀ   📲 " ,url=f"https://telegram.me/share/url?url=https://telegram.me/RahulReviews?start={message.from_user.id}") ]   ])
     await message.reply_text(f"ʏᴏᴜ  ᴡɪʟʟ  ɢᴇᴛ  100 ᴍʙ  ᴇxᴛʀᴀ  ᴀᴛ  ᴇᴠᴇʀʏ  ʀᴇꜰᴇʀ",reply_to_message_id = message.id,reply_markup=reply_markup,)
     
