cb196e6f4470ff69816176bca8504105f8f1e9ae
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-26 14:35:35 +0530

    Update and rename refer.py to ping.py

--- plugins/refer.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
-@Client.on_message(filters.private & filters.command(["refer"]))
-async def refer(client,message):
-    reply_markup = InlineKeyboardMarkup(
-       		[ [ InlineKeyboardButton(" 📲   ᴄʟɪᴄᴋ  ᴍᴇ  ᴛᴏ  ʀᴇꜰᴇʀ   📲 " ,url=f"https://telegram.me/share/url?url=https://telegram.me/RahulReviewsYT") ]   ])
-    await message.reply_text(f"ʏᴏᴜ  ᴡɪʟʟ  ɢᴇᴛ  100 ᴍʙ  ᴇxᴛʀᴀ  ᴀᴛ  ᴇᴠᴇʀʏ  ʀᴇꜰᴇʀ",reply_to_message_id = message.id,reply_markup=reply_markup,)
-    
