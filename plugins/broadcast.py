3571b57856f5fcd1132f3f21b17525da30de310f
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-26 14:20:53 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,31 +1,29 @@
-import os
 from pyrogram.errors import FloodWait
 import asyncio
-from pyrogram import Client ,filters
-from helper.database import getid ,delete
+from pyrogram import Client, filters
+from helper.database import getid, delete
 import time
-ADMIN = int(os.environ.get("ADMIN"))
- 
+from config import *
 
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
+@Client.on_message(filters.private & filters.user(OWNER) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
- if (message.reply_to_message):
-   ms = await message.reply_text("Geting All ids from database ...........")
-   ids = getid()
-   tot = len(ids)
-   success = 0 
-   failed = 0 
-   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
-   for id in ids:
-     try:
-     	time.sleep(1)
-     	await message.reply_to_message.copy(id)
-     	success += 1 
-     except:
-     	failed += 1
-     	delete({"_id":id})     	 
-     	pass
-     try:
-     	await ms.edit( f"᚛› 𝐓𝐨𝐭𝐚𝐥 𝐔𝐬𝐞𝐫𝐬 - {tot}\n᚛› 𝐂𝐨𝐦𝐩𝐥𝐞𝐭𝐞𝐝 - {success}\n᚛› 𝐁𝐥𝐨𝐜𝐤𝐞𝐝 - {failed}" )
-     except FloodWait as e:
-     	await asyncio.sleep(t.x)
+    if (message.reply_to_message):
+        ms = await message.reply_text("Getting All IDs From Database..\nPlease Wait")
+        ids = getid()
+        tot = len(ids)
+        success = 0
+        failed = 0
+        await ms.edit(f"Starting Broadcast... \nSending Message To {tot} Users")
+        for id in ids:
+            try:
+                time.sleep(1)
+                await message.reply_to_message.copy(id)
+                success += 1
+            except:
+                failed += 1
+                delete({"_id": id})
+                pass
+            try:
+                await ms.edit(f"Message Sent To {success} Chats. \n\n{failed} Chats Failed On Receiving Message. \n\nTotal - {tot}")
+            except FloodWait as e:
+                await asyncio.sleep(t.x)
