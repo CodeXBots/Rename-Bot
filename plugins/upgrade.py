45a7822cfca92ce9bc7ed13901b75ab77d4798ec
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-26 14:43:17 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -3,99 +3,57 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-	text = """
-𝐁𝐮𝐲   𝐏𝐥𝐚𝐧   𝐀𝐜𝐜.   𝐓𝐨   𝐘𝐨𝐮𝐫   𝐍𝐞𝐞𝐝 !!!
-
-
-𝟏.   𝐅𝐫𝐞𝐞 𝐏𝐥𝐚𝐧
-
-👉     ᴅᴀɪʟʏ  ʟɪᴍɪᴛ 2 ɢʙ
-👉     ꜰʀᴇᴇ
-
-
-𝟐.   𝐁𝐚𝐬𝐢𝐜  𝐏𝐥𝐚𝐧
-
-👉     ᴅᴀɪʟʏ  ʟɪᴍɪᴛ  10 ɢʙ
-👉     ʙᴜʏ  <a href='https://telegra.ph/SUPPORT-12-22-2'>₹ 49/ᴍᴏɴᴛʜ.</a> 
+	text = """**Free Plan User**
+	Daily  Upload limit 2GB
+	Price 0
 	
-
-𝟑.   𝐒𝐭𝐚𝐧𝐝𝐚𝐫𝐝  𝐏𝐥𝐚𝐧 
-
-👉     ᴅᴀɪʟʏ  ʟɪᴍɪᴛ  50 ɢʙ
-👉     ʙᴜʏ  <a href='https://telegra.ph/SUPPORT-12-22-2'>₹ 99/ᴍᴏɴᴛʜ.</a>  
+	**🪙 Basic** 
+	Daily  Upload  limit 20GB
+	Price Rs 49  ind /🌎 0.59$  per Month
 	
-
-𝟒.   𝐏𝐫𝐞𝐦𝐢𝐮𝐦  𝐏𝐥𝐚𝐧 
-
-👉     ᴅᴀɪʟʏ  ʟɪᴍɪᴛ  100 ɢʙ
-👉     ʙᴜʏ  <a href='https://telegra.ph/SUPPORT-12-22-2'>₹ 199/ᴍᴏɴᴛʜ.</a>
-
-•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
-
-𝐍𝐎𝐓𝐄 :  ᴀꜰᴛᴇʀ  ᴘᴀʏᴍᴇɴᴛ  ꜱᴇɴᴅ  ꜱᴄʀᴇᴇɴꜱʜᴏᴛ  ᴛᴏ  ᴏᴡɴᴇʀ.
-"""
-	keybord = InlineKeyboardMarkup(
-                [
-                    [
-            InlineKeyboardButton('ᴄᴏɴᴛᴀᴄᴛ  ᴛᴏ  ᴏᴡɴᴇʀ  ❣️',url='https://telegram.me/CodeXBro')
-            ],
-                    [
-            InlineKeyboardButton('ʙᴜʏ   ꜱᴜʙꜱᴄʀɪᴘᴛɪᴏɴ   😊',url='https://telegra.ph/SUPPORT-12-22-2')
-            ],
-                    [
-                        InlineKeyboardButton('🔮  ʜᴇʟᴘ',url='https://telegram.me/CodeXSupport'),
-                        InlineKeyboardButton("🏠   ʜᴏᴍᴇ", callback_data = "cancel")
-                    ]
-                ]
-            )
-	await update.message.edit(text = text, disable_web_page_preview=True, reply_markup = keybord)
+	**⚡ Standard**
+	Daily Upload limit 50GB
+	Price Rs 99  ind /🌎 1.19$  per Month
+	
+	**💎 Pro**
+	Daily Upload limit 100GB
+	Price Rs 179  ind /🌎 2.16$  per Month
+	
+	
+	Pay Using Upi I'd `RahulReviews@sbi`
+	
+	After Payment Send Screenshots Of 
+        Payment To Admin @CodeXBro"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("Admin",url = "https://telegram.me/CodeXBro")], 
+        			[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """
-𝐁𝐮𝐲   𝐏𝐥𝐚𝐧   𝐀𝐜𝐜.   𝐓𝐨   𝐘𝐨𝐮𝐫   𝐍𝐞𝐞𝐝 !!!
-
-
-𝟏.   𝐅𝐫𝐞𝐞 𝐏𝐥𝐚𝐧
-
-👉     ᴅᴀɪʟʏ  ʟɪᴍɪᴛ 2 ɢʙ
-👉     ꜰʀᴇᴇ
-
-
-𝟐.   𝐁𝐚𝐬𝐢𝐜  𝐏𝐥𝐚𝐧
-
-👉     ᴅᴀɪʟʏ  ʟɪᴍɪᴛ  10 ɢʙ
-👉     ʙᴜʏ  <a href='https://telegra.ph/SUPPORT-12-22-2'>₹ 49/ᴍᴏɴᴛʜ.</a> 
+	text = """**Free Plan User**
+	Daily  Upload limit 2GB
+	Price 0
 	
-
-𝟑.   𝐒𝐭𝐚𝐧𝐝𝐚𝐫𝐝  𝐏𝐥𝐚𝐧 
-
-👉     ᴅᴀɪʟʏ  ʟɪᴍɪᴛ  50 ɢʙ
-👉     ʙᴜʏ  <a href='https://telegra.ph/SUPPORT-12-22-2'>₹ 99/ᴍᴏɴᴛʜ.</a>  
+	**🪙 Basic** 
+	Daily  Upload  limit 20GB
+	Price Rs 49  ind /🌎 0.59$  per Month
 	
-
-𝟒.   𝐏𝐫𝐞𝐦𝐢𝐮𝐦  𝐏𝐥𝐚𝐧 
-
-👉     ᴅᴀɪʟʏ  ʟɪᴍɪᴛ  100 ɢʙ
-👉     ʙᴜʏ  <a href='https://telegra.ph/SUPPORT-12-22-2'>₹ 199/ᴍᴏɴᴛʜ.</a>
-
-•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
-
-𝐍𝐎𝐓𝐄 :  ᴀꜰᴛᴇʀ  ᴘᴀʏᴍᴇɴᴛ  ꜱᴇɴᴅ  ꜱᴄʀᴇᴇɴꜱʜᴏᴛ  ᴛᴏ  ᴏᴡɴᴇʀ.
-"""
-	keybord = InlineKeyboardMarkup(
-                [
-                    [
-            InlineKeyboardButton('ᴄᴏɴᴛᴀᴄᴛ  ᴛᴏ  ᴏᴡɴᴇʀ  ❣️',url='https://telegram.me/CodeXBro')
-            ],
-                    [
-            InlineKeyboardButton('ʙᴜʏ   ꜱᴜʙꜱᴄʀɪᴘᴛɪᴏɴ   😊',url='https://telegra.ph/SUPPORT-12-22-2')
-            ],
-                    [
-                        InlineKeyboardButton('🔮  ʜᴇʟᴘ',url='https://telegram.me/CodeXSupport'),
-                        InlineKeyboardButton("🏠   ʜᴏᴍᴇ", callback_data = "cancel")
-                    ]
-                ]
-            )
-	await message.reply_text(text = text, disable_web_page_preview=True, reply_markup = keybord)
+	**⚡ Standard**
+	Daily Upload limit 50GB
+	Price Rs 99  ind /🌎 1.19$  per Month
+	
+	**💎 Pro**
+	Daily Upload limit 100GB
+	Price Rs 179  ind /🌎 2.16$  per Month
+	
+	
+	Pay Using Upi I'd `RahulReviews@sbi`
+	
+	After Payment Send Screenshots Of 
+        Payment To Admin @CodeXBro"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("Admin",url = "https://telegram.me/CodeXBro")], 
+        			[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = text,reply_markup = keybord)
