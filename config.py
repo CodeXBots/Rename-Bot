7d4e7c899cbe562c644c0f05939f668ad354b39a
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-09-03 12:09:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,27 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+# Required Variables Config
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
 
+
+# Premium 4GB Renaming Client Config
+STRING_SESSION = os.environ.get("STRING_SESSION", "")
+
+
+# Log & Force Channel Config
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "CodeXBots")
 
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ada3f739fed7efdbe7b08.jpg")
+# Mongo DB Database Config
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "CodeXBots")
+
+
+# Other Variables Config
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ada3f739fed7efdbe7b08.jpg")
