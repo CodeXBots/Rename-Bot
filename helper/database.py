4e23118a1a18148bb89830a1b1535b079bc63838
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-09-03 11:36:35 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -2,15 +2,19 @@ import pymongo
 import os
 from helper.date import add_date
 from config import *
-mongo = pymongo.MongoClient(DB_URL)
-db = mongo[DB_NAME]
+mongo = pymongo.MongoClient(DATABASE_URL)
+db = mongo[DATABASE_NAME]
 dbcol = db["user"]
 
 
+
+# Total User
 def total_user():
     user = dbcol.count_documents({})
     return user
 
+
+# Insert Bot Data
 def botdata(chat_id):
     bot_id = int(chat_id)
     try:
@@ -20,19 +24,24 @@ def botdata(chat_id):
         pass
 
 
+# Total Renamed Files
 def total_rename(chat_id, renamed_file):
     now = int(renamed_file) + 1
     dbcol.update_one({"_id": chat_id}, {"$set": {"total_rename": str(now)}})
 
 
+# Total Renamed File Size
 def total_size(chat_id, total_size, now_file_size):
     now = int(total_size) + now_file_size
     dbcol.update_one({"_id": chat_id}, {"$set": {"total_size": str(now)}})
 
+
+# Insert User Data
 def insert(chat_id):
     user_id = int(chat_id)
     user_det = {"_id": user_id, "file_id": None, "caption": None, "daily": 0, "date": 0,
-                "uploadlimit": 10737418240, "used_limit": 0, "usertype": "Free", "prexdate": None}
+                "uploadlimit": 5368709120, "used_limit": 0, "usertype": "Free", "prexdate": None,
+                "metadata": False, "metadata_code": "By @RahulReviewsYT"}
     try:
         dbcol.insert_one(user_det)
     except:
@@ -40,50 +49,60 @@ def insert(chat_id):
         pass
 
 
+# Add Thumbnail Data
 def addthumb(chat_id, file_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": file_id}})
-
 
 def delthumb(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": None}})
 
 
+
+
+# ============= Metadata Function Code =============== #
+
+def setmeta(chat_id, bool_meta):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"metadata": bool_meta}})
+
+def setmetacode(chat_id, metadata_code):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"metadata_code": metadata_code}})
+
+# ============= Metadata Function Code =============== #
+
+
+
+# Add Caption Data
 def addcaption(chat_id, caption):
     dbcol.update_one({"_id": chat_id}, {"$set": {"caption": caption}})
-
 
 def delcaption(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"caption": None}})
 
 
+
 def dateupdate(chat_id, date):
     dbcol.update_one({"_id": chat_id}, {"$set": {"date": date}})
-
 
 def used_limit(chat_id, used):
     dbcol.update_one({"_id": chat_id}, {"$set": {"used_limit": used}})
 
-
 def usertype(chat_id, type):
     dbcol.update_one({"_id": chat_id}, {"$set": {"usertype": type}})
-
 
 def uploadlimit(chat_id, limit):
     dbcol.update_one({"_id": chat_id}, {"$set": {"uploadlimit": limit}})
 
 
+# Add Premium Data
 def addpre(chat_id):
     date = add_date()
     dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": date[0]}})
 
-
 def addpredata(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": None}})
 
-
 def daily(chat_id, date):
     dbcol.update_one({"_id": chat_id}, {"$set": {"daily": date}})
-
 
 def find(chat_id):
     id = {"_id": chat_id}
@@ -94,9 +113,18 @@ def find(chat_id):
             caption = i["caption"]
         except:
             caption = None
+        try:
+            metadata = i["metadata"]
+        except:
+            metadata = False
+        try:
+            metadata_code = i["metadata_code"]
+        except:
+            metadata_code = None
+            
 
-        return [file, caption]
 
+        return [file, caption, metadata, metadata_code]
 
 def getid():
     values = []
@@ -107,7 +135,6 @@ def getid():
 
 def delete(id):
     dbcol.delete_one(id)
-
 
 def find_one(id):
     return dbcol.find_one({"_id": id})
