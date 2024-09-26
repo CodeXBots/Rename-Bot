a0689f658d6b7fc92cf082d054cdb1a9b47f0194
CodeXBots <RahulReviewsYT@gmail.com>
2024-09-26 20:19:29 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -7,7 +7,7 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
     text = """**Free Plan User**
-Daily  Upload limit 2GB
+Daily  Upload limit 5GB
 Price 0
 
 **🪙 Basic**
@@ -39,7 +39,7 @@ After Payment Send Screenshots Of Payment To Admin @CodeXBro"""
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
     text = """**Free Plan User**
-Daily  Upload limit 2GB
+Daily  Upload limit 5GB
 Price 0
 
 **🪙 Basic**
